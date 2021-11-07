6991d28bb5c0e7e588dc5b48bbb6cc5d4964f787
Hossam Mohamed <hossammohamed26>
2021-11-07 23:24:45 +0200

    last

--- app/src/main/java/com/example/task1_20180308_20180131_20170090/MainActivity.java
+++ app/src/main/java/com/example/task1_20180308_20180131_20170090/MainActivity.java
@@ -3,22 +3,59 @@ package com.example.task1_20180308_20180131_20170090;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingService;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String CHANNEL_ID = "101";
+    private final  String TAG="FirebaseMessagingServic";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        FirebaseMessaging.getInstance().subscribeToTopic("topic");
+        FirebaseMessaging.getInstance().subscribeToTopic("topicbyhoss");
+        createNotificationChannel();
+        getToken();
+    }
+
+   private void getToken()
+    {
+        FirebaseMessaging.getInstance().getToken().addOnCompleteListener(new OnCompleteListener<String>() {
+            @Override
+            public void onComplete(@NonNull Task<String> task) {
+                if (!task.isSuccessful()) {
+                    Log.d(TAG, "onComplete: Failed to get the Token");
+                }
+
+                //Token
+                String token = task.getResult();
+                Log.d(TAG, "onComplete: " + token);
+            }
+        });
+
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "FirebaseMessagingServic";
+            String description = "Receve Firebase notification";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 
 
