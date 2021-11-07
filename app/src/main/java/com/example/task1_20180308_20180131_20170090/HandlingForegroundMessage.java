8ce270c850ecf3a55322ba9b62805b47143fb996
nadahossamm <63651482+nadahossamm@users.noreply.github.com>
2021-11-07 19:54:50 +0200

    Initial commit

---
+++ app/src/main/java/com/example/task1_20180308_20180131_20170090/HandlingForegroundMessage.java
@@ -0,0 +1,20 @@
+package com.example.task1_20180308_20180131_20170090;
+
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class HandlingForegroundMessage extends FirebaseMessagingService {
+   private final  String TAG="FirebaseMessagingServic";
+@Override
+    public void onMessageReceived(RemoteMessage remoteMessage)
+{
+
+
+
+}
+
+
+
+}
---
+++ app/src/main/java/com/example/task1_20180308_20180131_20170090/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.task1_20180308_20180131_20170090;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FirebaseMessaging.getInstance().subscribeToTopic("topic");
+    }
+
+
+}
+
