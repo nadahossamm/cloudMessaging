e74c9ca93d3712ac6fa619e8fec97874a7709a44
nadahossamm <63651482+nadahossamm@users.noreply.github.com>
2021-11-14 22:40:52 +0200

    nada hossam

--- app/src/main/java/com/example/task1_20180308_20180131_20170090/HandlingForegroundMessage.java
+++ app/src/main/java/com/example/task1_20180308_20180131_20170090/HandlingForegroundMessage.java
@@ -8,25 +8,41 @@ import androidx.annotation.NonNull;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class HandlingForegroundMessage extends FirebaseMessagingService {
     private static final String CHANNEL_ID = "101";
     private final  String TAG="FirebaseMessagingServic";
-    FirebaseDatabase rootNode;
-    DatabaseReference reference;
+    DatabaseReference dbref;
 @Override
+
     public void onMessageReceived(@NonNull RemoteMessage remoteMessage)
     {
-        Log.d(TAG, "onMessageReceived: "+remoteMessage.getNotification().getTitle());
+        Log.d(TAG, "onMessageReceived from: "+remoteMessage.getNotification().getTitle());
         Log.d(TAG, "onMessageReceived: "+remoteMessage.getNotification().getBody());
+        addtodb(remoteMessage.getNotification().getTitle(),remoteMessage.getNotification().getBody());
 
-
+        showNotification(remoteMessage.getNotification().getTitle(),remoteMessage.getNotification().getBody());
     }
 
+    public void addtodb (String body, String title)
+    {
+
+        Map<String, Object> message = new HashMap<>();
+        message.put("title", title);
+        message.put("body", body);
+         FirebaseDatabase.getInstance().getReference("Notifications").setValue(message);
+    }
     private void showNotification(String title,String message){
         Intent intent = new Intent(this, MainActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
@@ -40,20 +56,12 @@ public class HandlingForegroundMessage extends FirebaseMessagingService {
                 // Set the intent that will fire when the user taps the notification
                 .setContentIntent(pendingIntent)
                 .setAutoCancel(true);
-        /*creating an instance of the notifications class*/
-        String notificationTitle = title;
-        String notificationMessage = message;
-        Notifications notification= new Notifications(notificationTitle,notificationMessage);
 
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
 
-
         // notificationId is a unique int for each notification that you must define
         notificationManager.notify(1, builder.build());
-        /*sending the created instance upon sending*/
-        rootNode = FirebaseDatabase.getInstance();
-        reference = rootNode.getReference("Notofications");
-        reference.setValue(notification);
+     //   addtodb(message,title);
     }
 
 
--- app/src/main/java/com/example/task1_20180308_20180131_20170090/MainActivity.java
+++ app/src/main/java/com/example/task1_20180308_20180131_20170090/MainActivity.java
@@ -12,23 +12,36 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.messaging.FirebaseMessaging;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String CHANNEL_ID = "101";
     private final  String TAG="FirebaseMessagingServic";
-
+  //  DatabaseReference dbref;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        FirebaseMessaging.getInstance().subscribeToTopic("topicbyhoss");
+        //addtodb("nadaaaa","testt");
+        FirebaseMessaging.getInstance().subscribeToTopic("topic");
         createNotificationChannel();
         getToken();
+
+
     }
 
    private void getToken()
@@ -61,5 +74,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-}
+//    public void addtodb (String body, String title)
+//    {
+//
+//        Map<String, Object> message = new HashMap<>();
+//        message.put("title", title);
+//        message.put("body", body);
+//        dbref= FirebaseDatabase.getInstance().getReference().child("Notifications");
+//        dbref.push().setValue(message);
+//    }
+
+
+    }
 
